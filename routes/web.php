a262921ccb87a1ed6cba726f351f67a9ad3815b7
virak0001 <virakcambodia44@gmail.com>
2021-12-11 01:05:32 +0700

    chore: comfig user

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('login',function() {
+    return view('signin');
+});
+Route::get('register',function() {
+    return view('signup');
+});
+Route::get('verify',function() {
+    return view('verify');
+});
